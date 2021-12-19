d08d7440c9c31d7f6f6fad530ea91fd4db55fed6
rajatkumartgpta <rajatkumarnic2300@gmail.com>
2021-12-19 17:25:42 +0530

    Added Three Files

---
+++ WhiskeyDispenser.py
@@ -0,0 +1,8 @@
+# Whiskey Dispenser
+# print("Enter your Age : ")
+# age = int(input())
+
+# if(age>21):
+#     print("Here's your drink!")
+# else:
+#     print("Fuck off!!")
---
+++ main.py
@@ -0,0 +1,8 @@
+x = "good bank  "
+y = "provide better service "
+def myfunc():
+   x = "101 old bank "
+   print ("csb bank is "+ x )
+   print("csb bank is " + y)
+myfunc()
+print("Csb bank is "+ x+y)   
